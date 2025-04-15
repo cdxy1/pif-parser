e6382d074c540cb1ce05fbcc914b9ecdf4b19212
Admin <cdxy@list.ru>
2025-04-15 21:05:41 +0300

    Upd install scripts

--- install.ps1
+++ install.ps1
@@ -1,1 +1,1 @@
-pyinstaller --paths ".\.venv" --onefile .\src\pif_parser\__main__.py --name pchola --icon .\src\pif_parser\gui\icon.jpg
+pyinstaller --noconsole --paths ".\.venv" --onefile .\src\pif_parser\__main__.py --name pchola --icon .\icon\icon.ico
